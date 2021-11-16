619b003738d8aa565272f125acb37fac2d86f0c6
ArifGlory <arifglory46@gmail.com>
2021-11-16 14:55:44 +0700

    add tukang pake kategori

--- app/src/main/java/com/tapisdev/caritukang/activity/admin/AddTukangActivity.kt
+++ app/src/main/java/com/tapisdev/caritukang/activity/admin/AddTukangActivity.kt
@@ -13,6 +13,8 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -26,6 +28,7 @@ import com.tapisdev.mysteam.model.Kategori
 import com.tapisdev.mysteam.model.Tukang
 import kotlinx.android.synthetic.main.activity_add_kategori.*
 import kotlinx.android.synthetic.main.activity_add_tukang.*
+import kotlinx.android.synthetic.main.activity_register_tukang.*
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.util.*
@@ -46,8 +49,12 @@ class AddTukangActivity : BaseActivity(),PermissionHelper.PermissionListener {
     var lon = 0.0
 
     var TAG_GET_KATEGORI = "getKategori"
-    lateinit var adapter: AdapterKategori
     var listKategori = ArrayList<Kategori>()
+    var listNamaKategori = ArrayList<String>()
+    lateinit var adapterSpinnerKategori : ArrayAdapter<String>
+    var selectedIdKategori = ""
+
+    lateinit var adapter: AdapterKategori
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,11 +64,9 @@ class AddTukangActivity : BaseActivity(),PermissionHelper.PermissionListener {
 
         permissionHelper = PermissionHelper(this)
         permissionHelper.setPermissionListener(this)
+        adapter = AdapterKategori(listKategori)
 
 
-        ivBackCreateTukang.setOnClickListener {
-            onBackPressed()
-        }
         btnLokasi.setOnClickListener {
             val i = Intent(this,SelectMapsActivity::class.java)
             startActivity(i)
@@ -72,7 +77,73 @@ class AddTukangActivity : BaseActivity(),PermissionHelper.PermissionListener {
         rlImageTukang.setOnClickListener {
             launchGallery()
         }
+
+        adapterSpinnerKategori = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, listNamaKategori)
+        spKategori.adapter = adapterSpinnerKategori
+        spKategori.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                var pos = position+1
+                Log.d(TAG_GET_KATEGORI,"nama kategori : "+listNamaKategori.get(position))
+                Log.d(TAG_GET_KATEGORI,"id kategori : "+listKategori.get(position).id_kategori)
+                selectedIdKategori = listKategori.get(position).id_kategori
+
+                /*if (!namaKategori.equals("Pilih Kategori")){
+                    val i = Intent(requireContext(), ResultActivity::class.java)
+                    i.putExtra("jenis","provinsi")
+                    i.putExtra("key",namaProvinsi)
+                    i.putExtra("value",namaProvinsi)
+                    startActivity(i)
+                }*/
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        })
+
+        getDataKategori()
     }
+
+    fun getDataKategori(){
+        showLoading(this)
+        kategoriRef.whereEqualTo("active",1)
+            .get().addOnSuccessListener { result ->
+                dismissLoading()
+                listKategori.clear()
+                //Log.d(TAG_GET_Sparepart," datanya "+result.documents)
+                for (document in result){
+                    //Log.d(TAG_GET_Sparepart, "Datanya : "+document.data)
+                    var kategori : Kategori = document.toObject(Kategori::class.java)
+                    kategori.id_kategori = document.id
+                    listKategori.add(kategori)
+
+                }
+                if (listKategori.size == 0){
+                    showInfoMessage("Kategori kosong..")
+                }
+                adapter.notifyDataSetChanged()
+
+                for (c in 0 until listKategori.size){
+                    var kategori  = listKategori.get(c)
+                    listNamaKategori.add(kategori.nama_kategori.toString())
+                }
+                adapterSpinnerKategori.notifyDataSetChanged()
+
+                if (listNamaKategori.size != 0){
+                    selectedIdKategori = listKategori.get(0).id_kategori
+                }
+
+            }.addOnFailureListener { exception ->
+                showErrorMessage("terjadi kesalahan : "+exception.message)
+                Log.d(TAG_GET_KATEGORI,"err : "+exception.message)
+            }
+    }
+
 
     fun checkValidation(){
         var getName = edNamaTukang.text.toString()
@@ -103,8 +174,12 @@ class AddTukangActivity : BaseActivity(),PermissionHelper.PermissionListener {
                 getAlamat,
                 getjamkerja,
                 lat.toString(),
-                lon.toString()
-
+                lon.toString(),
+                selectedIdKategori,
+                "",
+                0,
+                0,
+                1
             )
             uploadFoto()
         }
